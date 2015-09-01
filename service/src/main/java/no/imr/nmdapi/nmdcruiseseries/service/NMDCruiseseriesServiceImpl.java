f6ede46558ec6cbb26d0b473837083fd0d303d08
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-01 16:24:02 +0200

    Updated service layer and dao version

--- service/src/main/java/no/imr/nmdapi/nmdcruiseseries/service/NMDCruiseseriesServiceImpl.java
+++ service/src/main/java/no/imr/nmdapi/nmdcruiseseries/service/NMDCruiseseriesServiceImpl.java
@@ -1,11 +1,13 @@
 package no.imr.nmdapi.nmdcruiseseries.service;
 
+import java.io.File;
 import java.util.List;
 import no.imr.commons.nmdcruiseseries.domain.v1.CruiseSerieType;
-import no.imr.nmdapi.dao.file.NMDDataDao;
+import no.imr.nmdapi.dao.file.NMDSeriesReferenceDao;
 import no.imr.nmdapi.exceptions.BadRequestException;
 import no.imr.nmdapi.generic.response.v1.ListElementType;
 import no.imr.nmdapi.generic.response.v1.ResultElementType;
+import org.apache.commons.configuration.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -15,17 +17,25 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class NMDCruiseseriesServiceImpl implements NMDCruiseseriesService {
 
+    /**
+     * Data type.
+     */
+    private static final String TYPE = "cruiseseries";
+
     @Autowired
-    private NMDDataDao nmdDataDao;
+    private NMDSeriesReferenceDao seriesReferenceDao;
+
+    @Autowired
+    private Configuration configuration;
 
     @Override
     public Object getData(final String name) {
-        return nmdDataDao.get(name, CruiseSerieType.class);
+        return seriesReferenceDao.get(name, CruiseSerieType.class.getPackage().getName());
     }
 
     @Override
     public void deleteData(final String name) {
-        nmdDataDao.delete(name);
+        seriesReferenceDao.delete(TYPE, name, true);
     }
 
    @Override
@@ -33,17 +43,20 @@ public class NMDCruiseseriesServiceImpl implements NMDCruiseseriesService {
         if (!name.equalsIgnoreCase(cruiseserie.getCruiseseriename())) {
             throw new BadRequestException("Cruiseserie name is not equal to value in the data.");
         }
-        nmdDataDao.insert(name, cruiseserie, CruiseSerieType.class);
+        String readRole = configuration.getString("default.readrole");
+        String writeRole = configuration.getString("default.writerole");
+        String owner = configuration.getString("default.owner");
+        seriesReferenceDao.insert(writeRole, readRole, owner, TYPE, name, cruiseserie, true);
     }
 
     @Override
     public void updateData(final String name, final CruiseSerieType cruiseserie) {
-        nmdDataDao.update(name, cruiseserie, CruiseSerieType.class);
+        seriesReferenceDao.update(name, cruiseserie);
     }
 
     @Override
     public ListElementType list() {
-        List<String> names = nmdDataDao.listSeries();
+        List<String> names = seriesReferenceDao.list();
         ListElementType elementType = new ListElementType();
         for (String name : names) {
             ResultElementType resultElementType = new ResultElementType();
