982148bdf63579c3a8e76707def9a5825aa0aa4f
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-03 15:34:42 +0200

    Fixed access descision voter

--- web/src/main/java/no/imr/nmdapi/nmdcruiseseries/security/access/voters/CruiseseriesAccessDecisionVoter.java
+++ web/src/main/java/no/imr/nmdapi/nmdcruiseseries/security/access/voters/CruiseseriesAccessDecisionVoter.java
@@ -1,7 +1,11 @@
 package no.imr.nmdapi.nmdcruiseseries.security.access.voters;
 
 import java.util.Collection;
+import java.util.HashSet;
+import no.imr.nmdapi.dao.file.NMDSeriesReferenceDao;
 import no.imr.nmdapi.nmdcruiseseries.controller.CruiseseriesController;
+import org.apache.commons.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.access.AccessDecisionVoter;
 import static org.springframework.security.access.AccessDecisionVoter.ACCESS_ABSTAIN;
@@ -9,6 +13,7 @@ import static org.springframework.security.access.AccessDecisionVoter.ACCESS_DEN
 import static org.springframework.security.access.AccessDecisionVoter.ACCESS_GRANTED;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.stereotype.Service;
@@ -22,10 +27,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class CruiseseriesAccessDecisionVoter implements AccessDecisionVoter<FilterInvocation> {
 
-    /**
-     * Role to have to gain write access.
-     */
-    private static final String ROLE_WRITE_ACCESS = "SG-FAG-430-NMD";
+    @Autowired
+    private NMDSeriesReferenceDao seriesReferenceDao;
+
+    @Autowired
+    private Configuration configuration;
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
@@ -40,21 +46,43 @@ public class CruiseseriesAccessDecisionVoter implements AccessDecisionVoter<Filt
     @Override
     public int vote(Authentication auth, FilterInvocation obj, Collection<ConfigAttribute> confAttrs) {
         if (obj.getFullRequestUrl().contains(CruiseseriesController.CRUISESERIES_URL)) {
-            if (obj.getHttpRequest().getMethod().equalsIgnoreCase(HttpMethod.GET.name()) || obj.getHttpRequest().getMethod().equalsIgnoreCase(HttpMethod.HEAD.name())) {
-                // Read access.
-                return ACCESS_GRANTED;
-            } else {
-                // Write access.
-                if (auth.isAuthenticated() && auth.getAuthorities().contains(new SimpleGrantedAuthority(CruiseseriesAccessDecisionVoter.ROLE_WRITE_ACCESS))) {
+            if (obj.getHttpRequest().getMethod().equalsIgnoreCase(HttpMethod.POST.name())) {
+                if (auth.isAuthenticated() && auth.getAuthorities().contains(new SimpleGrantedAuthority(configuration.getString("default.writerole")))) {
                     return ACCESS_GRANTED;
                 } else {
                     return ACCESS_DENIED;
                 }
+            } else if (obj.getHttpRequest().getMethod().equalsIgnoreCase(HttpMethod.PUT.name()) || obj.getHttpRequest().getMethod().equalsIgnoreCase(HttpMethod.DELETE.name())) {
+                Collection<String> auths = getAuths(auth.getAuthorities());
+                String[] args = obj.getRequestUrl().split("/");
+                if (auth.isAuthenticated() && seriesReferenceDao.hasWriteAccess(auths, "cruiseseries", args[1])) {
+                    return ACCESS_GRANTED;
+                } else {
+                    return ACCESS_DENIED;
+                }
+            } else if (obj.getHttpRequest().getMethod().equalsIgnoreCase(HttpMethod.GET.name())) {
+                Collection<String> auths = getAuths(auth.getAuthorities());
+                String[] args = obj.getRequestUrl().split("/");
+                if (seriesReferenceDao.hasReadAccess(auths, "cruiseseries", args[1])) {
+                    return ACCESS_GRANTED;
+                } else {
+                    return ACCESS_DENIED;
+                }
+            } else {
+                return ACCESS_GRANTED;
             }
         } else {
             // Not reference data.
             return ACCESS_ABSTAIN;
         }
+    }
+
+    private Collection<String> getAuths(Collection<? extends GrantedAuthority> auths) {
+        Collection<String> authsStr = new HashSet<String>();
+        for (GrantedAuthority authority : auths) {
+            authsStr.add(authority.getAuthority());
+        }
+        return authsStr;
     }
 
 }
